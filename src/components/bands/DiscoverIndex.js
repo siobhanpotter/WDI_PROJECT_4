d63c8958846d6b6265df792e20f2739f2fad55ca
siobhanpotter <siobhanpotterone@gmail.com>
2017-12-15 12:03:01 +0000

    added some styling to index pages and nav

--- src/components/bands/DiscoverIndex.js
+++ src/components/bands/DiscoverIndex.js
@@ -2,6 +2,7 @@ import React    from 'react';
 import Axios    from 'axios';
 
 import Promise  from 'bluebird';
+// import SimpleSlider  from '../utility/SimpleSlider';
 import { Link } from 'react-router-dom';
 
 class DiscoverIndex extends React.Component {
@@ -24,31 +25,52 @@ class DiscoverIndex extends React.Component {
   render() {
 
     return (
-      <div>
-        <div>
-          {/* <div>
+
+
+
+
+      <div className="row">
+      å  {/* <div>
             <img src="https://static.pexels.com/photos/8263/pexels-photo.jpg" className="img-responsive"  />
           </div> */}
-          <div>
-            <img src="../assets/guitars.png" className="img-responsive"  />
-          </div>
-          <div className="section-one-outer">
-            <section className="container">
-              <h1>Musicians</h1>
-              { <Link to={'/users'} className="standard-button">
-                <i className="fa fa-pencil" aria-hidden="true"></i>View All
-              </Link>}
-            </section>
+
+
+
+        <div>
+          <div className="col-sm-12">
+            <div className="row">
+              <div className="section-one-outer col-lg-6">
+                <section className="container">
+                  <h1>Musicians</h1>
+                  { <Link to={'/users'} className="standard-button">
+                    <i className="fa fa-pencil" aria-hidden="true"></i>View All
+                  </Link>}
+                </section>
+              </div>
+            </div>
+
+            <div className="row">
+              <div className="section-two-outer col-lg-6">
+                <section className="container">
+                  <h1>Bands</h1>
+                  { <Link to={'/bands'} className="standard-button">
+                    <i className="fa fa-pencil" aria-hidden="true"></i>View   All
+                  </Link>}
+                </section>
+              </div></div>
           </div>
 
-          <div className="section-two-outer">
-            <section className="container">
-              <h1>Bands</h1>
-              { <Link to={'/bands'} className="standard-button">
-                <i className="fa fa-pencil" aria-hidden="true"></i>View   All
-              </Link>}
-            </section>
+          <div className="container" role="main">
+            <div className="row">
+              <div className="col-sm-4">
+                <div className="row">
+                </div>
+              </div>
+            </div>
           </div>
+
+
+
         </div>
       </div>
     );
