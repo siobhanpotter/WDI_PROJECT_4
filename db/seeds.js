a0497c1caf137d9e31d4925e1de991e8e6f39b86
siobhanpotter <siobhanpotterone@gmail.com>
2018-01-22 23:34:52 +0000

    test 1

--- db/seeds.js
+++ db/seeds.js
@@ -1,122 +1,39 @@
 // const mongoose = require('mongoose');
 // mongoose.Promise = require('bluebird');
-// const { db } = require('../config/environment');
-// const app = require('express')();
-// const environment = app.get('env');
 //
-// mongoose.connect(db[environment], { useMongoClient: true });
+// const { dbURI } = require('../config/environment');
+// mongoose.connect(dbURI, { useMongoClient: true });
 //
-// const User = require('../models/User');
-// const Band = require('../models/Band');
+// const Cheese = require('../models/cheese');
+// const Country = require('../models/country');
 //
-// //release model
-// User.collection.drop();
-// Band.collection.drop();
+// Cheese.collection.drop();
+// Country.collection.drop();
 //
-// //Add Member models here
-// User.create([
-//   {
-//     username: 'ed123',
-//     email: 'ed@ed.com',
-//     password: 'password',
-//     passwordConfirmation: 'password',
-//     image: 'asdfasd',
-//     about: 'asdfasdf',
-//     mainInstrument: 'asdfasdf',
-//     location: '51.465614, -0.114973',
-//     formatted_address: 'Brixton O2',
-//     style: 'Pop'
-//   },
-//   {
-//     email: 'tom@tom.com',
-//     username: 'tom123',
-//     password: 'password',
-//     passwordConfirmation: 'password',
-//     image: 'asdfasd',
-//     about: 'asdfasdf',
-//     mainInstrument: 'asdfasdf',
-//     location: '51.465614, -0.114973',
-//     formatted_address: 'Brixton O2',
-//     style: 'Pop'
-//   }
-// ])
-//   .then(Users => {
-//     console.log(`${Users.length} new Users added`);
-//     return Band.create([
-//       {
-//         bandName: 'Alt J',
-//         image:
-//           'http://ksassets.timeincuk.net/wp/uploads/sites/55/2016/07/alt-J-Live-At-Red-Rocks-01-1.jpg',
-//         about: 'sdfsdf',
-//         style: 'alsdkfsd',
-//         members: ['Dan', 'Sam', 'Jenny'],
-//         location: '51.543370, -0.151815',
-//         formatted_address: 'The Roundhouse',
-//         memberRequired: 'a;sldkjfadslkfa',
-//         createdBy: Users[0]
-//       },
-//       {
-//         bandName: 'Alt J',
-//         image:
-//           'http://ksassets.timeincuk.net/wp/uploads/sites/55/2016/07/alt-J-Live-At-Red-Rocks-01-1.jpg',
-//         about: 'sdfsdf',
-//         style: 'alsdkfsd',
-//         members: ['Dan', 'Sam', 'Jenny'],
-//         location: '51.543370, -0.151815',
-//         formatted_address: 'The Roundhouse',
-//         memberRequired: 'a;sldkjfadslkfa',
-//         createdBy: Users[0]
-//       },
-//       {
-//         bandName: 'Alt J',
-//         image:
-//           'http://ksassets.timeincuk.net/wp/uploads/sites/55/2016/07/alt-J-Live-At-Red-Rocks-01-1.jpg',
-//         about: 'sdfsdf',
-//         style: 'alsdkfsd',
-//         members: ['Dan', 'Sam', 'Jenny'],
-//         location: '51.543370, -0.151815',
-//         formatted_address: 'The Roundhouse',
-//         memberRequired: 'a;sldkjfadslkfa',
-//         createdBy: Users[0]
-//       },
-//       {
-//         bandName: 'Alt J',
-//         image:
-//           'http://ksassets.timeincuk.net/wp/uploads/sites/55/2016/07/alt-J-Live-At-Red-Rocks-01-1.jpg',
-//         about: 'sdfsdf',
-//         style: 'alsdkfsd',
-//         members: ['Dan', 'Sam', 'Jenny'],
-//         location: '51.543370, -0.151815',
-//         formatted_address: 'The Roundhouse',
-//         memberRequired: 'a;sldkjfadslkfa',
-//         createdBy: Users[0]
-//       },
-//       {
-//         bandName: 'Alt J',
-//         image:
-//           'http://ksassets.timeincuk.net/wp/uploads/sites/55/2016/07/alt-J-Live-At-Red-Rocks-01-1.jpg',
-//         about: 'sdfsdf',
-//         style: 'alsdkfsd',
-//         members: ['Dan', 'Sam', 'Jenny'],
-//         location: '51.543370, -0.151815',
-//         formatted_address: 'The Roundhouse',
-//         memberRequired: 'a;sldkjfadslkfa',
-//         createdBy: Users[0]
-//       },
-//       {
-//         bandName: 'Alt J',
-//         image:
-//           'http://ksassets.timeincuk.net/wp/uploads/sites/55/2016/07/alt-J-Live-At-Red-Rocks-01-1.jpg',
-//         about: 'sdfsdf',
-//         style: 'alsdkfsd',
-//         members: ['Dan', 'Sam', 'Jenny'],
-//         location: '51.543370, -0.151815',
-//         formatted_address: 'The Roundhouse',
-//         memberRequired: 'a;sldkjfadslkfa',
-//         createdBy: Users[0]
-//       }
-//     ]);
+// Country
+//   .create([{
+//     username: 'Canada',
+//     email: 'Canada',
+//     password: 'Canada',
+//     image: 'Canada',
+//     about: 'Canada',
+//     mainInstrument: 'Canada',
+//     location: 'Canada',
+//     formatted_address: 'Canada',
+//     style:
+//   }])
+//   .then((countries) => {
+//     console.log(`${countries.length} countries created!`);
+//
+//     return Cheese
+//       .create([{
+//         name: 'Gorgonzola',
+//         country: countries[15],
+//         tastingNotes: 'Gorgonzola is traditionally a rich creamy cheese, but the blue-green ripples add a sharp spicy flavor that provides an excellent contrast to its richness. The taste ranges from mild to sharp, depending on age.',
+//         strength: 3,
+//         image: 'http://www.monthlyclubs.com/media/catalog/product/cache/13/image/285x/9df78eab33525d08d6e5fb8d27136e95/g/o/gorgonzola-1.jpg'
+//       }]);
 //   })
-//   .then(bands => console.log(`${bands.length} Cool bands near you!`))
+//   .then(cheese => console.log(`${cheese.length} cheeses created!`))
 //   .catch(err => console.log(err))
 //   .finally(() => mongoose.connection.close());
