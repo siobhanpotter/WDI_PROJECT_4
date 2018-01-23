eb53aa2e135ca548acdcfb0dd2268e7c775df967
siobhanpotter <siobhanpotterone@gmail.com>
2018-01-23 13:44:00 +0000

    padding added

--- src/components/bands/DiscoverIndex.js
+++ src/components/bands/DiscoverIndex.js
@@ -27,7 +27,7 @@ class DiscoverIndex extends React.Component {
     return (
       <div className="row">
         <div className="discover-container">
-          <div className="col-sm-6 col-lg-6 ">
+          <div className="col-sm-12 col-md-6 col-lg-6 ">
             <div className="row discover-left main-header-wrapper">
               <h1 className="main-header">Music Box</h1>
               <h4>Music Box is a way to bring local musicians and bands together.</h4>
@@ -39,27 +39,31 @@ class DiscoverIndex extends React.Component {
               </ul>
             </div>
             <div>
-              <div className="row discover-left musicians-div">
-                <h1>Musicians</h1>
-                { <Link to={'/users'} className="standard-button">
-                  <i className="fa fa-pencil" aria-hidden="true"></i>View All
-                </Link>}
-            </div>
+              <div className="row discover-left musicians-div container">
+                <div className="container padding-left">
+                  <h1>Musicians</h1>
+                  { <Link to={'/users'} className="standard-button">
+                    <i className="fa fa-pencil" aria-hidden="true"></i>View All
+                  </Link>}
+                </div>
+              </div>
 
-            <div className="row discover-left bands-div">
-              <h1>Bands</h1>
-              { <Link to={'/bands'} className="standard-button">
-              <i className="fa fa-pencil" aria-hidden="true"></i>View   All
-            </Link>}
-          </div>
-        </div>
+              <div className="row discover-left bands-div">
+                <div className="container padding-left">
+                  <h1>Bands</h1>
+                  { <Link to={'/bands'} className="standard-button">
+                    <i className="fa fa-pencil" aria-hidden="true"></i>View   All
+                  </Link>}
+                </div>
+              </div>
+            </div>
           </div>
 
           {/* <div className="img-container">
             <img src={band.image} className="img-band" />
           </div> */}
 
-          <div className="col-sm-6 col-lg-6">
+          <div className="col-sm-12 col-md-6 col-lg-6">
             <div className="disc-img-container">
               <img src="../assets/couple.png" className="img-responsive img-band"/>
             </div>
